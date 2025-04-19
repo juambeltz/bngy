ad1b3da5802488a41d1e1464a6ec588acf936cc1
Juan Juambeltz <juan.juambeltz@agesic.local>
2025-04-19 15:32:58 -0300

    Cambio en home.jsx para la versión test en Vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
       <div>
-        <h1>Bienvenido a Binaguy</h1>
+        <h1>Bienvenidos sean a Binaguy</h1>
         <p>¡Aquí encontrarás los mejores autos!</p>
       </div>
     );
